08a234969626fe9bd1b1c0f301ab593ca113abc6
Manveet Kaur <manveet660@gmail.com>
2025-08-28 23:15:47 +0800

    created databse...yet to clean up

--- app.py
+++ app.py
@@ -1,17 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__,template_folder="app/templates")
+app.config['SECRET_KEY'] = 'yoursecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-    @app.route('/')
-    def home():
-        return render_template('index.html')
+# Flask-Login setup
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
-    return app
+# User model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
-from app import create_app
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-app = create_app()
+# Routes
+@app.route('/')
+@login_required
+def home():
+    return render_template("home.html", name=current_user.username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for("home"))
+        else:
+            flash("Invalid username or password")
+    return render_template("login.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash("Username already exists")
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account created! You can now log in.")
+            return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()   # Creates database if not exists
     app.run(debug=True)
