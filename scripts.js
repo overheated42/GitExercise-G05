b27517fb694ca66a96a1443b05ed8802bd3e0b77
Thadchaiyini <thadchaiyiniramesh@gmail.com>
2025-09-07 00:30:55 +0800

    adding campus paths

--- scripts.js
+++ scripts.js
@@ -17,19 +17,13 @@ map.setMaxBounds(bounds);
 map.setMinZoom(16);   // stop zooming too far out
 map.setMaxZoom(19);   // limit zoom in
 
-
-
-L.marker([2.927953649184701, 101.64216861623053]).addTo(map)
-  .bindPopup("Campus Center")
-  .openPopup();
-
-  fetch('campus_paths.geojson')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      style: {
-        color: "blue",
-        weight: 4
-      }
-    }).addTo(map);
-  });
+// Dashed path
+var path = L.polyline([
+  [2.928260982179367, 101.64158677651704],
+  [2.928171748808012, 101.64181511549168],
+  [2.927864389363691, 101.6419210118276]
+], {
+  color: '#ff9999',
+  weight: 1,
+  dashArray: "5,5"
+}).addTo(map);
