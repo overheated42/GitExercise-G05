2cdad660088dd5943e663e4ca51a2192448a99a5
Thadchaiyini <thadchaiyiniramesh@gmail.com>
2025-09-11 17:17:26 +0800

    whatever

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // center of the map
-var map = L.map('map').setView([2.927953649184701, 101.64216861623053], 17);
+var map = L.map('map').setView([2.927953649184701, 101.642168616230531], 17);
 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,9 +9,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // like how much users can zoom in
 var bounds = [
-  [2.925007050745863, 101.64058962504915],
-  [2.930762314451626, 101.64356106174803]
+[2.925007050745863, 101.64058962504915],
+[2.930762314451626, 101.64356106174803]
 ];
+
 // restrict like how much only the users can view
 map.setMaxBounds(bounds);
 map.setMinZoom(16);   // stop zooming too far out
@@ -27,7 +28,64 @@ fetch("campus_paths.geojson")
         dashArray: "2,8" // dashed line
       }
     }).addTo(map);
+    window.campusPaths = data;
   });
+
+  // Load campus places from GeoJSON
+let campusPlaces = {};
+let campusPolylines = [];
+let routingControl = null;
+let userMarker = null;
+
+ // Load campus paths
+ fetch("campus_paths.geojson")
+ .then(res => res.json())
+ .then(data => {
+   var campusLayer = L.geoJSON(data, {
+     style: { color: "red", weight: 3 }
+   }).addTo(map);
+
+   var campusPolylines = [];
+   campusLayer.eachLayer(l => {
+     if (l instanceof L.Polyline) campusPolylines.push(l);
+   });
+
+
+ fetch("campus_places.geojson")
+  .then(res => res.json())
+  .then(data => {
+    data.features.forEach(feature => {
+      let name = feature.properties.name;
+      let coords = feature.geometry.coordinates.slice().reverse(); // [lat, lng]
+      campusPlaces[name] = coords;
+
+      // Optional: add markers for each place
+      L.marker(coords).addTo(map).bindPopup(name);
+    });
+  });
+
+   // Custom router for campus paths
+   var customRouter = {
+    route: function(waypoints, callback) {
+      var line = [];
+      waypoints.forEach(wp => {
+        var closest = L.GeometryUtil.closest(map, campusPolylines, wp.latLng);
+        line.push(closest);
+      });
+      var lineRoute = L.polyline(line, { color: 'blue', weight: 5 });
+
+      callback(null, [{
+        name: "Campus Route",
+        coordinates: line,
+        instructions: [],
+        summary: { totalDistance: 0, totalTime: 0 },
+        inputWaypoints: waypoints,
+        waypoints: line.map(c => L.latLng(c)),
+        bounds: lineRoute.getBounds()
+      }]);
+    }
+  };
+
 
 // Marker for user's location
 let userMarker;
@@ -45,12 +103,38 @@ function onLocationFound(e) {
     }
 }
 
-function onLocationError(e) {
-    alert("Unable to retrieve your location. Please allow location access.");
-}
 
 map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
+map.on('locationerror', () => alert("Location access denied"));
 
 // Start tracking user's location
 map.locate({ setView: true, watch: true, maxZoom: 18 });
+
+    // Search bar
+    var geocoder = L.Control.geocoder({
+      defaultMarkGeocode: false
+    })
+    .on('markgeocode', function(e) {
+      var destLatLng = e.geocode.center;
+
+      // Remove old route if exists
+      if (routingControl) {
+        map.removeControl(routingControl);
+      }
+
+      // Draw new route
+      routingControl = L.Routing.control({
+        waypoints: [
+          userLatLng,
+          destLatLng
+        ],
+        router: customRouter,
+        routeWhileDragging: false,
+        show: false,
+        createMarker: function() { return null; }
+      }).addTo(map);
+    })
+    .addTo(map);
+  });
+  
+
