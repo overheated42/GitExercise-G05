e4e6ade0e7cdad055369437e695f0bee34e583c0
Thadchaiyini <thadchaiyiniramesh@gmail.com>
2025-09-17 11:11:53 +0800

    Refactor: Update campus places data, enhance navigation menu with dropdowns, and improve sidebar layout

--- app/app.py
+++ app/app.py
@@ -3,9 +3,7 @@ from app import create_app  # <-- use the one from __init__.py
 app = create_app()
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-
     app.run(debug=True) 
-=======
-    app.run(debug=True)
->>>>>>> f7d9d936088b84522c48d8466f3b79f4acdc1e51
+
+
+
