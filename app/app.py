191eec66e7e79232acfa68d9ca58db472c90a490
Lee Chun Hui <chunhuilee0118@gmail.com>
2025-09-27 12:13:33 +0800

    changed app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app  # <-- use the one from __init__.py
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host="0.0.0.0", port=port, debug=True)  # must bind to 0.0.0.0
